3254e568fdc373e2cb8d02e6730d4432ca4bae01
dagoquevedo <quevedo.dago@gmail.com>
2017-08-24 22:21:53 -0300

    Update P2

--- P2/P2_C.r
+++ P2/P2_C.r
@@ -1,12 +1,12 @@
-#Project : Cellular automaton - crystallization 1
+#Project : Cellular automaton - crystallization 2
 #Author  : Dago Quevedo
 #Date    : Aug 2017
 
 library(parallel)
 library(sna)
 
-unlink("P2_B*.png")
-unlink("P2_B*.gif")
+unlink("P2_C*.png")
+unlink("P2_C*.gif")
 
 args = commandArgs(trailingOnly = TRUE)
 
@@ -42,18 +42,22 @@ move = function(pos) {
 cluster = makeCluster(detectCores() - 1)
 clusterExport(cluster, "dim")
 
-state    = matrix(0, nrow = dim, ncol = dim)
-pos_seed = sample(1:num, seeds)
-
-for (seed in 1:seeds) {
-    state[pos_seed[seed]] = seed / seeds
-}
-
-generation = 0
+state       = matrix(0, nrow = dim, ncol = dim)
+seed        = 0
+generation  = 0
 
 repeat {
+    if(seed < seeds && any(state == 0)) {
+        seed     = seed + 1
+        pos_seed = round(runif(1, min = 1, max = num))
+        while(state[pos_seed] != 0) {
+            pos_seed = round(runif(1, min = 1, max = num))
+        }
+        state[pos_seed] = 1 - seed / seeds
+    }    
+
     clusterExport(cluster, "state")    
-    state      = matrix(parSapply(cluster,1:num,move),nrow=dim,ncol=dim,byrow = TRUE)
+    state = matrix(parSapply(cluster,1:num,move),nrow=dim,ncol=dim,byrow = TRUE)
 
     if(!any(state == 0)) {
         break;
@@ -61,23 +65,23 @@ repeat {
 
     generation = generation + 1
 
-    output = paste("B",formatC(generation,width=4,format="d",flag="0"),".png",sep="")    
+    output = paste("C",formatC(generation,width=4,format="d",flag="0"),".png",sep="")    
     png(output)
     plot.sociomatrix(state, diaglab = FALSE, 
                      main = sprintf("Generation: %d", generation), drawlab = FALSE)
-    graphics.off()
+    graphics.off()    
 }
 
 stopCluster(cluster)
 
-system(sprintf("convert -delay %d B*.png P2_B_%s.gif",20,'01'))
-unlink("B*.png")
+system(sprintf("convert -delay %d C*.png P2_C_%s.gif",20,'01'))
+unlink("C*.png")
 
 
 size_states = data.frame()
 size_states = rbind(size_states, as.vector(table(state)))
 
-png("P2_B_02.png")
+png("P2_C_02.png")
 boxplot(data.matrix(size_states), use.cols=FALSE, 
     xlab = "Seeds", ylab = "Size", main = "Seeds size")
 graphics.off()
@@ -97,7 +101,7 @@ for (i in border) {
 
 border_no_seed = table(state[!state %in% unique(border_value)])
 
-png("P2_B_03.png")
+png("P2_C_03.png")
 barplot(border_no_seed, main = "Seeds size without border", 
     xlab = "Seeds", ylab = "Size")
 graphics.off()
